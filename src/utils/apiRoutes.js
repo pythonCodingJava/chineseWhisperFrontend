d785003a0f530dfacc15f23021295182e000923a
Kartik <kartikgupta74665@gmail.com>
2024-06-30 16:26:09 +0530

    redirecting

--- src/utils/apiRoutes.js
+++ src/utils/apiRoutes.js
@@ -1,4 +1,4 @@
-const host = "https://chineseserver-4jwzf0ue.b4a.run/";
+const host = "http://localhost:3000/";
 export const register = `${host}auth/register`;
 export const login = `${host}auth/login`;
 export const content = `${host}content`;
