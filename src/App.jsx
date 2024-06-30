7816a421284beb9c705ef31f20e909528d2ed0cd
Kartik <kartikgupta74665@gmail.com>
2024-06-30 22:06:21 +0530

    fixed more bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Home from './pages/Home.jsx';
 
 function App() {
   const [token, useToken] = useState();
+  
   useEffect(()=>{
     var storedToken = sessionStorage.getItem("chineseWhisperToken");
     if(storedToken) useToken(JSON.parse(storedToken));
     console.log(JSON.parse(storedToken))
   },[])
+
   return(
     <BrowserRouter>
       <Home token={{token:token, setToken:useToken}}></Home>
---
+++ src/utils/apiRoutes.js
@@ -0,0 +1,12 @@
+const host = process.env.HOST_URL;
+export const register = `${host}auth/register`;
+export const login = `${host}auth/login`;
+export const content = `${host}content`;
+export const contentFromDate = `${host}content/from`;
+export const likeURL = `${host}content/action/like`;
+export const createURL = `${host}content/action/create`;
+export const getPost = `${host}content/fetch`;
+export const getReplies = `${host}content/replies`
+export const likeCmntURL = `${host}content/action/likecmnt`;
+export const addCmnt = `${host}content/action/comment`;
+export const logoutURL = `${host}content/action/logout`;
