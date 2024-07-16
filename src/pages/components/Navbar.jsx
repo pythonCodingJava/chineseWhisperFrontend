9b82ade011e76ff4467e3a9cc4a809936eaa5601
Kartik <kartikgupta74665@gmail.com>
2024-07-16 14:08:19 +0530

    made the ui feel faster

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,10 +1,5 @@
 import React, { useState, useEffect, useRef, lazy, Suspense } from "react";
-import {
-  Box,
-  Button,
-  Typography,
-  useMediaQuery,
-} from "@mui/material";
+import { Box, Button, Typography, useMediaQuery } from "@mui/material";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import styled from "styled-components";
 import Add from "@mui/icons-material/Add";
@@ -14,8 +9,8 @@ import LoadingBar from "react-top-loading-bar";
 import Favorite from "@mui/icons-material/Favorite";
 import ModeComment from "@mui/icons-material/ModeComment";
 
-const Login = lazy(()=>import("../Login"));
-const Register = lazy(()=>import("../Register"));
+const Login = lazy(() => import("../Login"));
+const Register = lazy(() => import("../Register"));
 
 const Diiv = styled.div`
   transition: opacity 0.5s;
@@ -31,7 +26,7 @@ function Navbar({
   setRetrieve,
   setLogin,
   likeNotif,
-  cmtNotif
+  cmtNotif,
 }) {
   const [appear, setAppear] = useState();
   const [register, setRegister] = useState();
@@ -43,41 +38,38 @@ function Navbar({
   const [appearCmt, setAppearCmt] = useState(false);
   const initRender = useRef();
 
-  useEffect(()=>{
+  useEffect(() => {
     let timeoutId;
-    if(initRender.current){
+    if (initRender.current) {
       setAppearLike(true);
       timeoutId = setTimeout(() => {
-      setAppearLike(false);
-    }, 2000);
-
+        setAppearLike(false);
+      }, 2000);
     }
-    
+
     // Cleanup function to clear the timeout if the component unmounts
     return () => clearTimeout(timeoutId);
-  },[likeNotif])
+  }, [likeNotif]);
 
-  useEffect(()=>{
+  useEffect(() => {
     let timeoutId;
-    if(initRender.current){
+    if (initRender.current) {
       setAppearCmt(true);
       timeoutId = setTimeout(() => {
-      setAppearCmt(false);
-    }, 2000);
-
-    } else{
+        setAppearCmt(false);
+      }, 2000);
+    } else {
       initRender.current = true;
     }
     // Cleanup function to clear the timeout if the component unmounts
     return () => clearTimeout(timeoutId);
-  },[cmtNotif])
+  }, [cmtNotif]);
 
   useEffect(() => {
     if (loading) {
       ref.current.continuousStart();
     } else {
       ref.current.complete();
-      setRedirect(true);
     }
   }, [loading]);
 
@@ -94,7 +86,7 @@ function Navbar({
       style={{ position: "fixed", width: "100vw", backgroundColor: "#0E1113" }}
     >
       <LoadingBar color="rgb(200,50,50)" ref={ref} />
-      {redirect ? <Navigate to="/"></Navigate> : <></>}
+      {redirect && <Navigate to="/" />}
       <Box
         sx={{
           height: "58px",
@@ -110,6 +102,8 @@ function Navbar({
           sx={{ "&:hover": { cursor: "pointer" } }}
           onClick={function () {
             setRetrieve((v) => !v);
+
+            setRedirect(true);
           }}
         >
           <div
@@ -210,32 +204,36 @@ function Navbar({
               </Box>
             </Link>
             <div style={{ display: "flex", justifyContent: "center" }}>
-              {appearLike && <Box
-                sx={{
-                  position: "absolute",
-                  top: "30px",
-                  bgcolor: "rgb(200,50,50)",
-                  borderRadius: "5px",
-                  p: "25px 8px 3px 8px",
-                  zIndex:1
-                }}
-                className="anim"
-              >
-                <Favorite />
-              </Box>}
-              {appearCmt && <Box
-                sx={{
-                  position: "absolute",
-                  top: "30px",
-                  bgcolor: "rgb(50,150,200)",
-                  borderRadius: "5px",
-                  p: "25px 8px 3px 8px",
-                  zIndex:1
-                }}
-                className="anim"
-              >
-                <ModeComment />
-              </Box>}
+              {appearLike && (
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "30px",
+                    bgcolor: "rgb(200,50,50)",
+                    borderRadius: "5px",
+                    p: "25px 8px 3px 8px",
+                    zIndex: 1,
+                  }}
+                  className="anim"
+                >
+                  <Favorite />
+                </Box>
+              )}
+              {appearCmt && (
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "30px",
+                    bgcolor: "rgb(50,150,200)",
+                    borderRadius: "5px",
+                    p: "25px 8px 3px 8px",
+                    zIndex: 1,
+                  }}
+                  className="anim"
+                >
+                  <ModeComment />
+                </Box>
+              )}
 
               <Box
                 sx={{
@@ -250,12 +248,11 @@ function Navbar({
                     bgcolor: "rgb(40,45,50)",
                     cursor: "pointer",
                   },
-                  zIndex:2
+                  zIndex: 2,
                 }}
                 onClick={notifTab}
               >
                 <NotificationsIcon />
-
               </Box>
             </div>
             <div style={{ marginRight: "15px" }}>
@@ -291,22 +288,22 @@ function Navbar({
       {register ? (
         <Suspense>
           <Register
-          func={setRegister}
-          login={() => {
-            setRegister(false);
-            setLogin(false);
-          }}
-        />
+            func={setRegister}
+            login={() => {
+              setRegister(false);
+              setLogin(false);
+            }}
+          />
         </Suspense>
       ) : login ? (
         <Suspense>
           <Login
-          func={token.setToken}
-          log={function () {
-            setLogin(false);
-          }}
-          register={setRegister}
-        />
+            func={token.setToken}
+            log={function () {
+              setLogin(false);
+            }}
+            register={setRegister}
+          />
         </Suspense>
       ) : (
         <></>
